f2c82516a1d96ab9d9ef791717c52c8a7964c27d
RoshanKC <roshankc.py@gmail.com>
2024-02-26 13:06:59 +0545

    practise day 1

---
+++ objectsInJs.js
@@ -0,0 +1,18 @@
+// dictionaries in js
+
+const codeAuthor = {
+    name : "Roshan KC",
+    age : 23,
+    address : "butwal"
+}
+
+console.log(codeAuthor.name)
+console.log(codeAuthor.name+" from "+codeAuthor.address+" age is "+codeAuthor.age)
+
+console.log(typeof(codeAuthor))
+
+console.log(" ")
+
+for(x in codeAuthor){
+    console.log(x)
+}
